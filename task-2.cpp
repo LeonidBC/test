d49c900e3d7d40f71017dc818beca90c2f19e11b
LeonidBC <157945217+LeonidBC@users.noreply.github.com>
2024-06-11 20:21:19 +0700

    Add files via upload

---
+++ task-2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+	float s, a;
+
+	printf( 'Разветвляющейся алгоритм \n' );
+	printf("Введите значение S: ");
+	scanf("%f", &s);
+	printf("Введите значение A: ");
+	scanf('%f', &a);
+
+	if (a <= 10)
+		s = s + a
+
+		printf("Resultat: ");
+
+	return 0;
+
+}
+
